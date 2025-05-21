dfab2de04bb169225cb376d7db621550262ae723
Adrian Sims <adriansims62781@gmail.com>
2025-05-21 22:24:28 +0200

    First Commit

---
+++ HelpParser/HelpDispatcher.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HelpParser;
+
+public static class HelpDispatcher
+{
+    public static void Dispatch(string[] args, List<HelpSection> sections, string toolName)
+    {
+        if (args.Length == 0 || args[0] != "help")
+        {
+            Console.WriteLine("Usage: help [command [subcommand ...]]");
+            return;
+        }
+
+        // Skip "help"
+        var commandPath = string.Join(" ", args.Skip(1));
+        HelpTextParser.PrintHelp(sections, commandPath);
+    }
+}
---
+++ HelpParser/Option.cs
@@ -0,0 +1,20 @@
+﻿#nullable enable
+
+public record Option
+{
+    public char? ShortOption { get; }
+    public string? LongOption { get; }
+    public int MaxOccurs { get; }
+    public int NumberOfParams { get; }
+    public string? Group { get; }
+
+    // internal constructor so we can use a builder
+    internal Option(char? shortOption, string? longOption, int maxOccurs, int numberOfParams, string? group)
+    {
+        ShortOption = shortOption;
+        LongOption = longOption;
+        MaxOccurs = maxOccurs;
+        NumberOfParams = numberOfParams;
+        Group = group;
+    }
+}
